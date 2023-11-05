0c0b701100a1ac6e0c445e75cfa8b150eb517d87
imikado@gmail.com <imikado@gmail.com>
2023-11-05 21:47:45 +0100

    add split/full viewport

---
+++ src/Levels/Level.gd
@@ -0,0 +1,71 @@
+extends Node
+
+@onready var players:={
+	
+	"player":{
+		viewport= $HBoxContainer/SubViewportContainer/SubViewport,
+		camera= $HBoxContainer/SubViewportContainer/SubViewport/Camera2D,
+		player= $HBoxContainer/SubViewportContainer/SubViewport/LevelTemplate/LevelWorld/Player
+		
+	},
+	"sheep":{
+		viewport= $HBoxContainer/SubViewportContainer2/SubViewport,
+		camera= $HBoxContainer/SubViewportContainer2/SubViewport/Camera2D,
+		player= $HBoxContainer/SubViewportContainer/SubViewport/LevelTemplate/LevelWorld/Sheep
+	}
+	
+}
+@onready var fullViewportContainer=$SubViewportContainerFull
+@onready var fullViewportViewport=$SubViewportContainerFull/SubViewport
+@onready var fullViewportCamera=$SubViewportContainerFull/SubViewport/Camera2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	players["sheep"].viewport.world_2d=players["player"].viewport.world_2d
+	for node in players.values():
+		var remote_transform:=RemoteTransform2D.new()
+		remote_transform.remote_path=node.camera.get_path()
+		remote_transform.use_global_coordinates=true
+		remote_transform.update_position=true
+		remote_transform.update_scale=false
+		node.player.add_child(remote_transform)
+		
+		
+	
+	fullViewportViewport.world_2d=players["player"].viewport.world_2d
+	var remote_transform:=RemoteTransform2D.new()
+	remote_transform.remote_path=fullViewportCamera.get_path()
+	remote_transform.use_global_coordinates=true
+	remote_transform.update_position=true
+	remote_transform.update_scale=false
+	players["player"].player.add_child(remote_transform)
+		
+	
+	
+func switchFull():
+	print('full')
+	fullViewportContainer.visible=true
+	fullViewportCamera.enabled=true
+	
+	
+	pass
+	
+func switchSplit():
+	print('split')
+	fullViewportContainer.visible=false
+	fullViewportCamera.enabled=true
+	pass
+		
+	
+func _process(delta):
+	if (
+		abs(players["player"].player.position.x - players["sheep"].player.position.x) < (512/2) 
+		and 
+		abs(players["player"].player.position.y - players["sheep"].player.position.y) < (50)
+	):
+		switchFull()
+	else:
+		switchSplit()
+	
+	pass
