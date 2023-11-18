65c3a8c0132b8167faff26cba13a8e4984db1237
imikado@gmail.com <imikado@gmail.com>
2023-11-18 20:12:05 +0100

    fix animation open/close conflict

--- src/Levels/Shared/bridge.gd
+++ src/Levels/Shared/bridge.gd
@@ -15,6 +15,8 @@ var is_open=true
 
 var init=false
 
+var _available=true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	
@@ -36,6 +38,9 @@ func _ready():
 	pass # Replace with function body.
 
 func open():
+	if(!_available):
+		return
+	_available=false
 	_subBridgeAnimation.play("open")
 	
 	is_open=true
@@ -43,22 +48,34 @@ func open():
 
 func sub_close():
 	if init:
+		_available=true
 		return
 	
 	var number=0
+	var tween = get_tree().create_tween()
+		
 	for child in _subBridgetParts.get_children():
 		number+=1
-		var tween = get_tree().create_tween()
-		tween.tween_property(child,'position', child.position+Vector2(-26*number,0.0),2.0)
+		tween.tween_property(child,'position', child.position+Vector2(-26*number,0.0),0.5).set_trans(Tween.TRANS_BACK)
 	
+	tween.connect("finished",bridge_tween_closed)
+	
+	
+func bridge_tween_closed():
 	init=true
+	_available=true
+	print('fini ouvrir')
 	
 	
 func sub_open():
+	_available=true
 	return
 		
 	
 func close():
+	if(!_available):
+		return
+	_available=false
 	_subBridgeAnimation.play("close")
 	
 	is_open=false
