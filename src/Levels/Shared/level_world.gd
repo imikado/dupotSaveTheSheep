cb30c8d7d881aa1c248fabd29be35468c9f1237a
imikado@gmail.com <imikado@gmail.com>
2023-11-26 14:52:25 +0100

    fix spawn

--- src/Levels/Shared/level_world.gd
+++ src/Levels/Shared/level_world.gd
@@ -10,17 +10,28 @@ static var _instance: World = null
 
 @onready var TRex=load("res://src/Actors/Enemies/TRex.tscn")
 
+@onready var spawnTimer:Timer=$spawnTimer
 
 func _ready():
 	_instance = self if _instance == null else _instance
+
+	spawn_trex()	
 	
-	for child in _spawnList.get_children():
-		var new_TRex=TRex.instantiate()
-		
-		_enemyList.add_child(new_TRex)
-		
-		new_TRex.global_position=child.global_position
+func spawn_trex():
 	
+	var spawnNumber=_spawnList.get_child_count()
+	var enemyNumber=_enemyList.get_child_count()
+	
+	if enemyNumber < spawnNumber*2: 
+		for child in _spawnList.get_children():
+			var new_TRex=TRex.instantiate()
+			
+			_enemyList.add_child(new_TRex)
+			
+			new_TRex.global_position=child.global_position
+
+
+	spawnTimer.start()
 	
 static func get_tile_data_at(position: Vector2) -> TileData:
 	var local_position: Vector2i = _instance.tile_map.local_to_map(position)
@@ -33,3 +44,9 @@ static func get_custom_data_at(position: Vector2, custom_data_name: String, defa
 		return defaultValue
 	return data.get_custom_data(custom_data_name)
 
+
+
+func _on_spawn_timer_timeout():
+	spawn_trex()
+	
+	
